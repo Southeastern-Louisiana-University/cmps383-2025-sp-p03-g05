bc58fdf3a94f7e022176d3d89284a574de67368b
Keira Stewart <stewartkeira5@gmail.com>
2025-03-11 23:14:23 -0500

    Basic showtime entity stuff

---
+++ Selu383.SP25.P03.Api/Features/Showtimes/ShowtimeDto.cs
@@ -0,0 +1,19 @@
+﻿using static Selu383.SP25.P03.Api.Features.Showtimes.Showtime;
+
+namespace Selu383.SP25.P03.Api.Features.Showtimes
+{
+    public class ShowtimeDto
+    {
+        public int Id { get; set; }
+        public TimeOnly Time { get; set; }
+        //the actual time (8:00 pm, 2:00 pm)
+        public DateOnly ShowDate { get; set; }
+        public int MovieId { get; set; }
+        public int TheaterId { get; set; }
+        public string Format { get; set; }
+        //formats like 3D, 2D, IMAX, etc.
+        public bool Available { get; set; }
+
+    }
+}
+
