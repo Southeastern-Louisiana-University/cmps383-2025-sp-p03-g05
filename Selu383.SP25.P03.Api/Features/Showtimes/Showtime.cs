5d71ff17b188857b8d328d341a78e5aa00095435
Keira Stewart <stewartkeira5@gmail.com>
2025-03-11 23:35:24 -0500

    Movie foreign key and list of showtimes
    Added explicit mapping of movie foreign key on the showtime entity and created a list of showtimes for a theater. Might change that later

--- Selu383.SP25.P03.Api/Features/Showtimes/Showtime.cs
+++ Selu383.SP25.P03.Api/Features/Showtimes/Showtime.cs
@@ -1,4 +1,7 @@
-﻿namespace Selu383.SP25.P03.Api.Features.Showtimes
+﻿using Selu383.SP25.P03.Api.Features.Theaters;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Selu383.SP25.P03.Api.Features.Showtimes
 {
     public class Showtime
     {
@@ -7,6 +10,7 @@
         public TimeOnly Time { get; set; }
         //the actual time (8:00 pm, 2:00 pm)
         public DateOnly ShowDate { get; set; }
+        [ForeignKey("Movie")]
         public int MovieId { get; set; }
         public int TheaterId { get; set; }
         public string Format { get; set; }
