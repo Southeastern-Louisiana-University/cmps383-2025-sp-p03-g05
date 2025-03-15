b11d558661b33e6b2a4703af0a7d7f63a677886c
carringtonwynn <carringtonwynn@gmail.com>
2025-03-15 18:17:08 -0500

    layout testing (home page)

---
+++ Selu383.SP25.P03.Web/src/Home.tsx
@@ -0,0 +1,25 @@
+import './Home.css'
+
+function Home() {
+    
+
+    return(
+        <>
+        <div className="topBanner">
+            
+        </div>
+        <div className = "middleBanner">
+
+        </div>
+        <div className = "bottomBanner">
+
+        </div>
+        
+        
+        
+        </>
+
+    )
+}
+
+export default Home;
