00f8fa78bc9bc8abdb416d93c259e20b5b45f627
Keira Stewart <stewartkeira5@gmail.com>
2025-03-28 09:44:43 -0500

    Commenting out unused imports

--- Selu383.SP25.P03.Web/src/main.tsx
+++ Selu383.SP25.P03.Web/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import ReactDOM from 'react-dom';
-import React from 'react';
+//import ReactDOM from 'react-dom';
+//import React from 'react';
 //import LoginPage from './LoginPage.tsx'
 //import RegisterPage from './RegisterPage'
 
