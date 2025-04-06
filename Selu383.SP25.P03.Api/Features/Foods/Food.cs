e07d4a096d23ee9c6c2bb13020cb47f37f1854ab
sonabhatta <sona.bhatta@selu.edu>
2025-04-06 11:37:03 -0500

    Modified Food.cs in Foods folder

--- Selu383.SP25.P03.Api/Features/Foods/Food.cs
+++ Selu383.SP25.P03.Api/Features/Foods/Food.cs
@@ -1,8 +1,11 @@
-﻿public class Food
+﻿namespace Selu383.SP25.P03.Api.Features.Foods
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string ImageUrl { get; set; }
-    public decimal Price { get; set; }
-}
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+
