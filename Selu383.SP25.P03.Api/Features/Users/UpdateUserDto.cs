257e9425cebfc186c0012da627474e4a71a843ed
Samuel <samuel.mettakadapa@selu.edu>
2025-04-07 09:52:38 -0500

    added UpdateUserDto and update user endpoint

---
+++ Selu383.SP25.P03.Api/Features/Users/UpdateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Selu383.SP25.P03.Api.Features.Users
+{
+    public class UpdateUserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
