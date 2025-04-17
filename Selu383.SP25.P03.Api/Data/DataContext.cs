713228b5e0b8c908efa5ab2c4836e3b665d42dc4
sonabhatta <sona.bhatta@selu.edu>
2025-04-17 09:58:27 -0500

    seat layout

--- Selu383.SP25.P03.Api/Data/DataContext.cs
+++ Selu383.SP25.P03.Api/Data/DataContext.cs
@@ -17,17 +17,22 @@ namespace Selu383.SP25.P03.Api.Data
         }
 
         public DbSet<Theater> Theaters { get; set; }
-        public DbSet<Seats> Seats { get; set; }           
+        public DbSet<Seats> Seats { get; set; }
         public DbSet<Ticket> Tickets { get; set; }
         public DbSet<Showtime> Showtimes { get; set; }
-        public DbSet<Food> Foods { get; set; }          
+        public DbSet<Food> Foods { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            var allSeats = new List<Seats>();
+            allSeats.AddRange(CreateSeatsForTheater(5, 150)); 
+            allSeats.AddRange(CreateSeatsForTheater(6, 200)); 
+            allSeats.AddRange(CreateSeatsForTheater(7, 300)); 
 
-     
+            builder.Entity<Seats>().HasData(allSeats);
+
             builder.Entity<UserRole>().HasKey(x => new { x.UserId, x.RoleId });
 
             builder.Entity<User>()
@@ -44,7 +49,6 @@ namespace Selu383.SP25.P03.Api.Data
                 .IsRequired()
                 .OnDelete(DeleteBehavior.Cascade);
 
-           
             builder.Entity<Seats>()
                 .Property(s => s.Row)
                 .IsRequired();
@@ -56,6 +60,41 @@ namespace Selu383.SP25.P03.Api.Data
             builder.Entity<Food>()
                 .Property(f => f.Price)
                 .HasColumnType("decimal(10,2)");
+        }
+
+        
+        private static List<Seats> CreateSeatsForTheater(int theaterId, int totalSeats)
+        {
+            var seats = new List<Seats>();
+            int numRows = 10;
+            int seatsPerRow = totalSeats / numRows;
+            int aisleCol = seatsPerRow / 2;
+
+            int seatId = theaterId * 1000;
+
+            for (int row = 0; row < numRows; row++)
+            {
+                char rowChar = (char)('A' + row);
+                int colNumber = 1;
+
+                for (int col = 0; col < seatsPerRow; col++)
+                {
+                    if (col == aisleCol)
+                        continue; 
+
+                    seats.Add(new Seats
+                    {
+                        Id = seatId++,
+                        TheaterId = theaterId,
+                        Row = rowChar.ToString(),
+                        Col = col,
+                        Number = colNumber++,
+                        IsTaken = false
+                    });
+                }
+            }
+
+            return seats;
         }
     }
 }
