935811348ac0131edef8446c6b9fd7014fa0b21f
sonabhatta <sona.bhatta@selu.edu>
2025-04-26 12:44:39 -0500

    Temporary fix for GitHub Actions

--- Selu383.SP25.P03.Api/Features/Food/Food.cs
+++ Selu383.SP25.P03.Api/Features/Food/Food.cs
@@ -6,9 +6,9 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
-
-
         public decimal Price { get; set; }
         public string Category { get; set; }
+
+        // Temporary comment to retrigger GitHub Action
     }
 }
